f4974ad7cf0caa5186781e0b0bd0ea353d526c8e
Khanh <khanhuh402@gmail.com>
2024-10-17 22:22:11 +0700

    Testing

--- src/main/webapp/cloud24.js
+++ src/main/webapp/cloud24.js
@@ -2,5 +2,5 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
-console.log("Testing");
+console.log("Hello");
 
