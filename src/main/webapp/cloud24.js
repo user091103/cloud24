803ad1e9547493ab7b3f37c3803a9f7dea93d7f9
Khanh <khanhuh402@gmail.com>
2024-10-23 13:50:55 +0700

    Showing time

--- src/main/webapp/cloud24.js
+++ src/main/webapp/cloud24.js
@@ -1,11 +1,11 @@
 
 
 let click_button = document.getElementById("click_button");
-let server_time = document.getElementById("server_time");
+let server_time = document.getElementById("cloud24/server_time");
 
 
 click_button.addEventListener("click", function () {
-    fetch('/server_time')
+    fetch('/cloud24/server_time')
             .then(response => response.text()) 
             .then(data => {
                 console.log(data); 
