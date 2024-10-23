20ee9163a67ab5db6ce27254e6a3432a27ca2079
Khanh <khanhuh402@gmail.com>
2024-10-23 12:56:29 +0700

    Showing time

--- src/main/webapp/cloud24.js
+++ src/main/webapp/cloud24.js
@@ -2,7 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
-const time = new Date().toLocaleString();
+const moment = 0;
+var time = moment.utc(new Date()).local().format('HH:mm');
 
 let click_button = document.getElementById("click_button");
 click_button.addEventListener("click", function () {
