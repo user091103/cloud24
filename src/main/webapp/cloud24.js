1006106e11a1ba212f4a29545346f607b4cd89bc
Khanh <khanhuh402@gmail.com>
2024-10-23 13:03:54 +0700

    Showing time

--- src/main/webapp/cloud24.js
+++ src/main/webapp/cloud24.js
@@ -2,14 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/JavaScript.js to edit this template
  */
-const moment = 0;
-var time = moment.utc(new Date()).local().format('HH:mm');
-
-let click_button = document.getElementById("click_button");
-click_button.addEventListener("click", function () {
-    alert('Took ' + (endTime - startTime) + 'ms');
-});
-
 var startTime = (new Date()).getTime(),
     endTime;
 
@@ -22,3 +14,8 @@ $.ajax({
     }
 });
 
+
+let click_button = document.getElementById("click_button");
+click_button.addEventListener("click", function () {
+    alert('Took ' + (endTime - startTime) + 'ms');
+});
