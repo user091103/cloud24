02e7f7ba2f0caa5d5a0da98c05fc9e246ece21e5
Khanh <khanhuh402@gmail.com>
2024-10-23 13:27:23 +0700

    Showing time

--- src/main/java/CountRows.java
+++ src/main/java/CountRows.java
@@ -1,6 +1,9 @@
+
 import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.OutputStreamWriter;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -15,11 +18,14 @@ public class CountRows extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        LocalTime currentTime = LocalTime.now(); // Sử dụng lớp LocalTime để lấy giờ hiện tại
+        String formattedTime = currentTime.format(DateTimeFormatter.ofPattern("HH:mm:ss")); // Định dạng thành "giờ:phút:giây"
+
         BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(resp.getOutputStream(), "UTF-8"));
-        
+
         resp.setContentType("text/plain");
         resp.setStatus(200);
-         resp.addDateHeader("time", System.currentTimeMillis());
+        writer.write("Server time: " + formattedTime);
         writer.write("Hi!");
         writer.flush();
         writer.close();
